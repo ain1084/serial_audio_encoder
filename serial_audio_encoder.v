00c4b23889b7d70ec78ee91a9f44d03b7ccea744
Seiji Ainoguchi <seiji.ainoguchi@gmail.com>
2021-04-03 01:25:31 +0900

    Fixed an issue where the left and right channels were swapped during underrun. Change signal name.   sclk -> clk   i_data -> i_audio.   osclk -> sclk   olrclr -> lrclk   osdat -> sdo Change parameter name (data_width -> audio_width)

--- serial_audio_encoder.v
+++ serial_audio_encoder.v
@@ -1,34 +1,34 @@
 `default_nettype none
 
-module serial_audio_encoder #(parameter data_width = 32)(
+module serial_audio_encoder #(parameter audio_width = 16)(
     input wire reset,
-    input wire sclk,
+    input wire clk,
     input wire is_i2s,
     input wire lrclk_polarity,
     input wire i_valid,
     output wire i_ready,
     input wire i_is_left,
-    input wire [data_width-1:0] i_data,
+    input wire [audio_width-1:0] i_audio,
     output reg is_underrun,
-    output wire osclk,
-    output wire olrclk,
-    output wire osdat);
+    output wire sclk,
+    output wire lrclk,
+    output wire sdo);
 
     reg reg_lrclk;
     reg [1:0] reg_sdata;
     reg is_next_left;
 
     reg is_valid_shift;
-    reg [data_width-2:0] shift;
-    reg [$clog2(data_width-1)-1:0] shift_count;
+    reg [audio_width-2:0] shift;
+    reg [$clog2(audio_width-1)-1:0] shift_count;
 
-    assign olrclk = reg_lrclk ^ lrclk_polarity;
-    assign osclk = ~sclk;
-    assign osdat = reg_sdata[is_i2s];
+    assign lrclk = reg_lrclk ^ lrclk_polarity;
+    assign sclk = ~clk;
+    assign sdo = reg_sdata[is_i2s];
 
     assign i_ready = !is_valid_shift;
 
-    always @(posedge sclk or posedge reset) begin
+    always @(posedge clk or posedge reset) begin
         if (reset) begin
             reg_lrclk <= 1'b1;  // Right channel at start
             is_next_left <= 1'b1;
@@ -42,20 +42,23 @@ module serial_audio_encoder #(parameter data_width = 32)(
                 shift_count <= shift_count - 1'b1;
                 is_valid_shift <= shift_count != 0;
                 shift <= shift << 1;
-                reg_sdata <= { reg_sdata[0], shift[data_width-2] };
+                reg_sdata <= { reg_sdata[0], shift[audio_width-2] };
                 is_underrun <= 1'b0;
             end else begin
                 if (i_valid && i_is_left == is_next_left) begin
                     is_next_left <= !is_next_left;
                     is_valid_shift <= 1'b1;
-                    shift <= i_data[data_width-2:0];
-                    shift_count <= data_width - 2;
+                    shift <= i_audio[audio_width-2:0];
+                    shift_count <= audio_width - 2;
                     reg_lrclk <= !reg_lrclk;
-                    reg_sdata <= { reg_sdata[0], i_data[data_width-1] };
+                    reg_sdata <= { reg_sdata[0], i_audio[audio_width-1] };
                     is_underrun <= 1'b0;
                 end else begin
-                    if (!is_underrun)
+                    // Underrun
+                    if (!is_underrun) begin
                         reg_lrclk <= !reg_lrclk;
+                        is_next_left <= !is_next_left;
+                    end
                     is_valid_shift <= 1'b0;
                     reg_sdata <= 2'b00;
                     is_underrun <= 1'b1;
--- test/serial_audio_encoder_loopback_tb.v
+++ test/serial_audio_encoder_loopback_tb.v
@@ -1,4 +1,5 @@
 `timescale 1ns/1ps
+`default_nettype none
 
 module serial_audio_encoder_loopback_tb();
 
@@ -6,7 +7,7 @@ module serial_audio_encoder_loopback_tb();
 
     localparam lrclk_polarity = 1'b1;
     localparam is_i2s = 1'b1;
-    localparam data_width = 32;
+    localparam audio_width = 32;
 
     initial begin
         $dumpfile("serial_audio_encoder_loopback_tb.vcd");
@@ -25,52 +26,53 @@ module serial_audio_encoder_loopback_tb();
     reg i_valid;
     wire i_ready;
     reg i_is_left;
-    reg [data_width-1:0] i_data;
-
-    wire sclk;
-    wire lrclk;
-    wire sdat;
-
+    reg [audio_width-1:0] i_audio;
 
     wire o_decoder_valid;
     wire o_decoder_is_left;
     wire [31:0] o_decoder_audio;
+
+    wire sclk;
+    wire lrclk;
+    wire sdo;
+
+    serial_audio_encoder #(.audio_width(audio_width)) audio_encoder_ (
+        .reset(reset),
+        .clk(Clock),
+        .is_i2s(is_i2s),
+        .lrclk_polarity(lrclk_polarity),
+        .i_valid(i_valid),
+        .i_ready(i_ready),
+        .i_is_left(i_is_left),
+        .i_audio(i_audio),
+        .is_underrun(),
+        .sclk(sclk),
+        .lrclk(lrclk),
+        .sdo(sdo)
+    );
+
     serial_audio_decoder audio_decoder_ (
         .reset(reset),
         .sclk(sclk),
-        .lrclk(lrclk),
-        .sdin(sdat),
-        .is_i2s(is_i2s),
         .lrclk_polarity(lrclk_polarity),
+        .lrclk(lrclk),
+        .sdin(sdo),
+        .is_i2s(is_i2s),
         .is_error(),
         .o_valid(o_decoder_valid),
         .o_ready(1'b1),
         .o_is_left(o_decoder_is_left),
         .o_audio(o_decoder_audio)
     );
-    serial_audio_encoder #(.data_width(data_width)) audio_encoder_ (
-        .reset(reset),
-        .sclk(Clock),
-        .is_i2s(is_i2s),
-        .lrclk_polarity(lrclk_polarity),
-        .i_valid(i_valid),
-        .i_ready(i_ready),
-        .i_is_left(i_is_left),
-        .i_data(i_data),
-        .is_underrun(),
-        .osclk(sclk),
-        .olrclk(lrclk),
-        .osdat(sdat)
-    );
 
     integer encoder_count = 0;
     integer decoder_count = 0;
 
-    task set_data(input is_left, input [data_width-1:0] data);
+    task set_audio(input is_left, input [audio_width-1:0] audio);
         begin
             i_valid <= 1'b1;
             i_is_left <= is_left;
-            i_data <= data;
+            i_audio <= audio;
             wait (i_ready) @(posedge Clock);
             //$write("encoder(%08h): is_left(%1b) data = %08h\n", encoder_count, i_is_left, i_data);
             encoder_count = encoder_count + 1;
@@ -81,17 +83,16 @@ module serial_audio_encoder_loopback_tb();
 
     initial begin
         i_valid = 1'b0;
-        i_data = 1'b0;
+        i_audio = 1'b0;
         i_is_left = 1'b0;
         reset = 1'b1;
         repeat (2) @(posedge Clock) reset = 1'b1;
         repeat (2) @(posedge Clock) reset = 1'b0;
 
-        set_data(1'b1, 32'hAAA7AAA3);
-        set_data(1'b0, 32'hAAA80AA4);
-        set_data(1'b1, 32'hAAA9AAA5);
-        set_data(1'b0, 32'hAAAA0AA6);
-
+        set_audio(1'b1, 32'hAAA7AAA3);
+        set_audio(1'b0, 32'hAAA80AA4);
+        set_audio(1'b1, 32'hAAA9AAA5);
+        set_audio(1'b0, 32'hAAAA0AA6);
 /* 
         set_data(1'b1, 16'h1234);
         set_data(1'b0, 16'h5556);
